cdb8076bb156ee0a41a56f2fbf6340b035ecc23c
Fabio Machado <fabiohsmachado@gmail.com>
2014-07-31 22:35:34 -0300

    Initial commit with the Dataset class.

---
+++ ParseDataset.py
@@ -0,0 +1,26 @@
+class Dataset:
+ def __init__(self):
+  self.data = [];
+  self.variablesQuantity = 0;
+  self.variablesCardinality = [];
+
+def ParserFile(filename):
+ 
+
+def main():
+    # OK: Create dataset class
+    # Parse file and create a dataset object
+    # Cross validate the dataset into subdatasets
+    # Creat subdirectory structure
+    # Save the subdataset to files
+    # Run scoring in each training dataset
+    # Run gobnilp foreach score file
+    # Run twilp for each score file
+    # Create BNTree class
+    # Parser gobnilp result and create a BNTree instance
+    # Parser twilp result and create another BNTree instance
+    # Apply MLE in the test instances of datasets with the Gobnilp BNTree
+    # Apply MLE in the test instances of datasets with the Twilp BNTree
+
+if __name__ == "__main__":
+ main();
